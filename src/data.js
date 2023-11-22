423b6ea7d93c97e159487d96216f2cfaf0d1adfb
abdukhalilovislom <abdukhalilovislom2021@gmail.com>
2023-11-22 16:42:28 +0500

    feat:url

--- src/data.js
+++ src/data.js
@@ -1,2 +1,2 @@
-export const base_url = "http://localhost:4100";
-// export const base_url = "https://project-back-sryf.onrender.com";
+// export const base_url = "http://localhost:4100";
+export const base_url = "https://course-project-6tty.onrender.com";
